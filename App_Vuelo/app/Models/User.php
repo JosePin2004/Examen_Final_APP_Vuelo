180a7bf10177b710e8a94966fac0d1bc7566138a
neyjurado <ney.jurado@ister.edu.ec>
2025-12-04 09:42:28 -0500

    configuracion login

--- App_Vuelo/app/Models/User.php
+++ App_Vuelo/app/Models/User.php
@@ -6,10 +6,12 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
     use HasFactory, Notifiable;
+    use HasApiTokens, HasFactory, Notifiable;
 
     /**
      * The attributes that are mass assignable.
@@ -19,7 +21,8 @@ class User extends Authenticatable
     protected $fillable = [
         'name',
         'email',
-        'password',
+        'password', 
+        'role',
     ];
 
     /**
--- App_Vuelo/routes/api.php
+++ App_Vuelo/routes/api.php
@@ -11,3 +11,16 @@ use App\Http\Controllers\Api\FlightController;
 
 // Ruta pública para ver el catálogo de vuelos
 Route::get('/flights', [FlightController::class, 'index']);
+
+use App\Http\Controllers\Api\AuthController;
+
+// Rutas Públicas de Autenticación
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+// Rutas Protegidas (Solo usuarios logueados pueden entrar aquí)
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/logout', [AuthController::class, 'logout']);
+    
+    // Aquí pondremos las reservas más adelante
+});
